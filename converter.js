3f2f222b5fdf215696fc62a2d3e63fe4f0cbbed7
Andile45 <manganyeandile@gmail.com>
2025-08-05 22:33:49 +0200

    i added js, css on the elements

--- converter.js
+++ converter.js
@@ -2,12 +2,12 @@ const calculate = document.getElementById("calculate");
 const output = document.getElementById("output");
 
 calculate.addEventListener("click", () => {
-  const fileSize = parseFloat(document.getElementById("fileSize").value());
-  const fileType = document.getElementById("fileType").value();
+  const fileSize = parseFloat(document.getElementById("fileSize").value);
+  const fileType = document.getElementById("fileType").value;
   const internetSpeed = parseFloat(
-    document.getElementById("Int-Speed").value()
+    document.getElementById("Int-Speed").value
   );
-  const intFormat = document.getElementById("int-speed-format").value();
+  const intFormat = document.getElementById("int-speed-format").value;
 
   let fileSizeInBits;
 
@@ -22,20 +22,48 @@ calculate.addEventListener("click", () => {
   }
 
   let speedBps;
-  if (intFormat === "Gbps") {
+  if (intFormat === "gbps") {
     speedBps = internetSpeed * 1_000_000_000; // Gbps to bps
-  } else if (intFormat === "Mbps") {
+  } else if (intFormat === "mbps") {
     speedBps = internetSpeed * 1_000_000; // Mbps to bps
   } else if (intFormat === "Kbps") {
     speedBps = internetSpeed * 1_000; // Kbps to bps
-  } else if (intFormat === "bps") {
-    speedBps = internetSpeed; // already bps
   } else {
     speedBps = internetSpeed * 1_000_000; // default Mbps
   }
 
   // Calculate transfer time in seconds
   const transferTime = fileSizeInBits / speedBps;
-  output.innerHTML = `Estimated Time : ${transferTime}`
+
+
+  output.innerHTML = `Estimated Time : ${transferTime} Seconds`
 
 });
+
+
+// Temparature js
+
+const tempConversion = document.getElementById("convert");
+const tempOutput = document.getElementById("tempOutput");
+
+tempConversion.addEventListener("click", ()=>{
+
+  const userTemp =parseFloat(document.getElementById("tempReading").value) ;
+  let convertedTp ;
+
+  const tempType = document.getElementById("temp-type").value;
+
+  if(tempType ==="cel"){
+     convertedTp = (userTemp *9/5)+32;
+     tempOutput.innerHTML = `The temparature is ${convertedTp} °F`
+  }else if(tempType ==="Fah"){
+    convertedTp = (userTemp - 32)*5/9;
+     tempOutput.innerHTML = `The temparature is ${convertedTp} °C`
+  };
+
+
+});
+
+
+// scientific js
+
